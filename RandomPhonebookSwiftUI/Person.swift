76fc7e1ec15e3382edc92c36f1252a570af83357
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-11-13 12:38:10 +0200

    Initial commit

---
+++ RandomPhonebookSwiftUI/Person.swift
@@ -0,0 +1,52 @@
+//
+//  Person.swift
+//  RandomPhonebookSwiftUI
+//
+//  Created by Vahtee Boo on 13.11.2021.
+//
+
+import Foundation
+
+struct Person {
+    
+    let name: String
+    let surname: String
+    let email: String
+    let phoneNumber: String
+    
+    var fullName: String {
+        "\(name) \(surname)"
+    }
+}
+
+extension Person {
+    static func getContactList() -> [Person] {
+        
+        var persons: [Person] = []
+        
+        let names = DataManager.shared.names.shuffled()
+        let surnames = DataManager.shared.surnames.shuffled()
+        let emails = DataManager.shared.emails.shuffled()
+        let phones = DataManager.shared.phones.shuffled()
+        
+        let iterationCount = min(names.count, surnames.count, emails.count, phones.count)
+        
+        for index in 0..<iterationCount {
+            let person = Person(
+                name: names[index],
+                surname: surnames[index],
+                email: emails[index],
+                phoneNumber: phones[index]
+            )
+            
+            persons.append(person)
+        }
+        
+        return persons
+    }
+}
+//
+//enum Contacts: String {
+//    case phone = "phone"
+//    case email = "tray"
+//}
